0f628f8aba41053eed4c4b7b18e147fc8fb30999
DM.Liou <dm.liou@gmail.com>
2023-03-28 21:28:42 +0800

    AI

---
+++ Bing-AI.py
@@ -0,0 +1,16 @@
+import random
+
+def guess_number():
+    secret_number = random.randint(1, 100)
+    while True:
+        user_guess = int(input("請猜一個1到100之間的數字："))
+        if user_guess == secret_number:
+            print("恭喜你！你猜對了！")
+            break
+        elif user_guess < secret_number:
+            print("太小了，請再猜一次。")
+        else:
+            print("太大了，請再猜一次。")
+
+
+guess_number()
